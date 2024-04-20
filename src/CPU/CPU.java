3e8ef57709caec4b750705e30c0abfedafc2c8da
Kiran-Ee <earnsha2@uwm.edu>
2024-04-19 20:47:53 -0500

    4/19/24 - KE - Notes + Schedule - Working on the design doc and will clean up everything during our meeting.

---
+++ src/CPU/CPU.java
@@ -0,0 +1,12 @@
+package CPU;
+
+// will keep track of ALL registers & maybe mem
+public class CPU {
+    private static String $a0 = "";
+    private static String $a1 = "";
+    private static String $a2 = "";
+
+    public CPU() {
+
+    }
+}
